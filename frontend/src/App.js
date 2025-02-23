9ddc444725e68d936ab2e7b7e3cf8b8021cfdfae
MadBoy <73926361+madboy482@users.noreply.github.com>
2025-02-23 17:43:23 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+import React from "react";
+import VehicleDashboard from "./components/VehicleDashboard";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <VehicleDashboard />
+    </div>
+  );
+}
+
+export default App;
+=======
+import React from "react";
+import VehicleDashboard from "./components/VehicleDashboard";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <VehicleDashboard />
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> d445825 (Esp32 and graph)
