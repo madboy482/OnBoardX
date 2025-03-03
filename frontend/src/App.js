6ef219373986ba3de9c72ce167da5a87ede5d277
MadBoy <73926361+madboy482@users.noreply.github.com>
2025-03-03 23:09:07 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import VehicleDashboard from "./components/VehicleDashboard";
 import "./index.css";
@@ -12,18 +11,3 @@ function App() {
 }
 
 export default App;
-=======
-import React from "react";
-import VehicleDashboard from "./components/VehicleDashboard";
-import "./index.css";
-
-function App() {
-  return (
-    <div className="App">
-      <VehicleDashboard />
-    </div>
-  );
-}
-
-export default App;
->>>>>>> d445825 (Esp32 and graph)
